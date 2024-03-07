4c98d51a5e9b62a436abe422ce137ba11cc26c14
Wanderson Castro <wandersonce1@hotmail.com>
2024-03-07 12:27:03 -0800

    Adding testing packages and adjusting the test file

--- src/tests/UpvoteList.test.tsx
+++ src/tests/UpvoteList.test.tsx
@@ -1,13 +1,19 @@
-import { render, screen, test, expect } from "@testing-library/react";
+import React from "react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import jest from "jest";
-import { useListStore } from "../store/ListStore";
-import UpvoteList from "../components/UpvoteList/UpvoteList";
+import UpvoteList from "../components/Upvote/UpvoteList";
 
 jest.mock("../store/ListStore");
 
 test("clicking the add button updates the selected state in the store", () => {
-  const initialUpvotes = [{ id: "item1", selected: false }];
+  const initialUpvotes = [
+    {
+      id: "list32",
+      parentId: "list1",
+      selected: false,
+    },
+  ];
+
   const listId = "list1";
 
   render(<UpvoteList id={listId} upvotes={initialUpvotes} />);
